bb05ee0eb3c9595cb2a2585adf7a2b29f66c09bd
Dmytro Tkachuk <geekhubme@gmail.com>
2023-05-02 16:45:49 +0300

    Clean code

--- src/main/java/com/example/universityschedule/controller/WelcomeController.java
+++ src/main/java/com/example/universityschedule/controller/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-    @GetMapping
+    @GetMapping("/")
     public String welcome() {
         return "welcome";
     }
